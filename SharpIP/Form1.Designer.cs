45413fa1315434dcba9672958e18598e1c3755c3
Mateus Moraes <mtu_h@hotmail.com>
2021-05-31 18:10:33 -0300

    Update Form1.Designer.cs

--- SharpIP/Form1.Designer.cs
+++ SharpIP/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace SharpIP
         {
             this.lbIpLocalAtual = new System.Windows.Forms.Label();
             this.lbIpLocalAtual_ = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbIpLocalAtual
@@ -51,11 +53,29 @@ namespace SharpIP
             this.lbIpLocalAtual_.TabIndex = 1;
             this.lbIpLocalAtual_.Text = "IP Local:";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(95, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lbIpLocalAtual_);
             this.Controls.Add(this.lbIpLocalAtual);
             this.Name = "Form1";
@@ -69,6 +89,8 @@ namespace SharpIP
 
         private System.Windows.Forms.Label lbIpLocalAtual;
         private System.Windows.Forms.Label lbIpLocalAtual_;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
